e105066710c0c5ec1a8ab1ad516253dc705ec6c8
devinci-it <vince.it@icloud.com>
2024-03-23 11:29:12 -0700

    chore(scripts/include_static.php):

--- scripts/include_static.php
+++ scripts/include_static.php
@@ -1,11 +1,12 @@
 <?php
+// scripts/include_static.php
 
 // Autoload the static assets
-include_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use Composer\Autoload\ClassLoader;
 
 // Register the asset directory
 $classLoader = new ClassLoader();
-$classLoader->add('Devinci\\Uicore\\', __DIR__ . '/../../../../../src/');
+$classLoader->addPsr4('Devinci\\Uicore\\', __DIR__ . '/../../src/UICore');
 $classLoader->register();
