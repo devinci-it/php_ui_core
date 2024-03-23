8823c4784761fe0c6fd252b240fb14c2b4149bb0
devinci-it <vince.it@icloud.com>
2024-03-23 11:33:04 -0700

    refactor(// scripts/include_static.php):

--- scripts/include_static.php
+++ scripts/include_static.php
@@ -1,12 +1,15 @@
 <?php
 // scripts/include_static.php
 
-// Autoload the static assets
-require_once __DIR__ . '/../vendor/autoload.php';
+// Define the base path of your library
+$basePath = __DIR__ . '/../';
+
+// Autoload the static assets from your library
+require_once $basePath . 'vendor/autoload.php';
 
 use Composer\Autoload\ClassLoader;
 
 // Register the asset directory
 $classLoader = new ClassLoader();
-$classLoader->addPsr4('Devinci\\Uicore\\', __DIR__ . '/../../src/UICore');
+$classLoader->addPsr4('Devinci\\Uicore\\', $basePath . 'src/UICore');
 $classLoader->register();
